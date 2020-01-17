02bba5cd283ebd42666eedff69597393736f9cd0
Hristo Shabanakov <Shabanakov.Hristo@outlook.com>
2020-01-17 18:36:02 +0100

    Adding more cookies and attributes.

--- C# Web Basics - January 2020/01.Web Server - Http Protocol/HttpServer/Program.cs
+++ C# Web Basics - January 2020/01.Web Server - Http Protocol/HttpServer/Program.cs
@@ -2,14 +2,15 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace HttpServer
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            const string NewLine = "\r\n";
+
             //Open a port
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 12345);
 
@@ -20,27 +21,46 @@ namespace HttpServer
             {
                 //Info about the client
                 TcpClient client = tcpListener.AcceptTcpClient();
-                using (NetworkStream stream = client.GetStream())
-                {
-                    var requestBytes = new byte[100000];
-                    //Returns the read bytes from the stream.
-                    var readBytes = stream.Read(requestBytes, 0, requestBytes.Length);
-                    //Encoding text to byte and byte to text.
-                    var stringRequest = Encoding.UTF8.GetString(requestBytes, 0, readBytes);
+                Task.Run(() => ProcessClient(client));
+            }
+        }
+        public static async Task ProcessClient(TcpClient client)
+        {
+            const string NewLine = "\r\n";
+            using (NetworkStream stream = client.GetStream())
+            {
+                byte[] requestBytes = new byte[100000];
+                //Returns the read bytes from the stream.
+                int readBytes = await stream.ReadAsync(requestBytes, 0, requestBytes.Length);
+                //Encoding text to byte and byte to text.
+                var stringRequest = Encoding.UTF8.GetString(requestBytes, 0, readBytes);
 
-                    Console.WriteLine(new string('=', 70));
-                    Console.WriteLine(stringRequest);
+                Console.WriteLine(new string('=', 70));
+                Console.WriteLine(stringRequest);
 
-                    string responseBody = "<h1> Hello, user</h1>";
-                       string response = "HTTP/1.0 200 OK" + NewLine + 
-                        "MyCustomServer/1.0" + NewLine +
-                        $"Content-Length: {responseBody.Length}" + NewLine + NewLine +
-                        responseBody;
+                //string responseBody = "<h1> Hello, user</h1>";
+                string responseBody = stringRequest;
+                string response = "HTTP/1.0 200 OK" + NewLine +
+                    "Content-Type: text/plain" + NewLine +
+                    //Domain attribute will enable cookies for all subdomains too.
+                    "Set-Cookie: cookie1=test1; Domain=localhost; Security;" + NewLine +
+                    //Without domain attribute cookies will be for the current site.
+                    "Set-Cookie: cookie2=test2; Max-Age=30" + NewLine +
+                    //Attributes Max-Age and Expires are for setting expiration time.
+                    "Set-Cookie: cookie3=expiredemo; Expires=30" +
+                    //Its important to use always DateTime.UtcNow to avoid time difference around the world.
+                    DateTime.UtcNow.AddDays(7).ToString("R") + NewLine +
+                    //Attribute Security will sent cookies only with https
+                    "Set-Cookie: cookie4=jsmalwareprotection; HttpOnly;" + NewLine +
+                    "Server: MyCustomServer/1.0" + NewLine +
+                    $"Content-Length: {responseBody.Length}" + NewLine + NewLine +
+                    responseBody;
 
-                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-                    stream.Write(responseBytes, 0, response.Length);
-                }
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                await stream.WriteAsync(responseBytes, 0, response.Length);
             }
         }
     }
 }
+
+
